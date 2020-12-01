463a491cb4aa3a2204d120895bae1b9ad54e5837
Russell Jones <jonesrussell42@gmail.com>
2020-12-01 15:10:28 +0000

    WIP

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -89,12 +89,9 @@
     <div class="flex flex-wrap items-center md:justify-between justify-center">
       <div class="w-full md:w-4/12 px-4 mx-auto text-center">
         <div class="text-sm text-gray-600 font-semibold py-1">
-          Copyright &copy;
+          &copy;
           {year}
-          Tailwind Starter Kit by
-          <a
-            href="https://www.creative-tim.com"
-            class="text-gray-600 hover:text-gray-900">Creative Tim</a>.
+          | Russell Jones
         </div>
       </div>
     </div>
