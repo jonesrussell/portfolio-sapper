297aecd6f131a19aa3bb94afca8e470685e74290
Russell Jones <jonesrussell42@gmail.com>
2020-12-21 20:40:44 +0000

    FAQ WIP

--- src/routes/faq.svelte
+++ src/routes/faq.svelte
@@ -12,5 +12,14 @@
     <p>12 factor is ....</p>
   </FaqItem>
 
+  <FaqItem question="What the heck is 'npm isntal'?">
+    <p>
+      Have a look at
+      <a
+        href="https://github.com/npm/cli/blob/2b8b9bdbc3606772dc7f7b3a95b7ba3c572b4e98/lib/utils/cmd-list.js#L26-L63">cmd-list.js</a>
+      on GitHub.
+    </p>
+  </FaqItem>
+
   <FaqItem question="What is PATH?">Bar</FaqItem>
 </FaqList>
