35019a80f9a742c4a4d9f0cec18294b02a26ab96
Russell Jones <jonesrussell42@gmail.com>
2021-02-17 11:23:40 -0500

    WIP Blog

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,10 +1,10 @@
-<script context="module" lang="ts">
-  export async function preload(this: any, _page: any, session: any) {
-    const { BLOG_URL } = session;
-    const res = await this.fetch(BLOG_URL);
-    const posts = await res.json();
-
-    return { posts };
+<script context="module">
+  export function preload({ params, query }) {
+    return this.fetch(`feed.json`)
+      .then((r) => r.json())
+      .then((posts) => {
+        return { posts };
+      });
   }
 </script>
 
@@ -30,17 +30,17 @@
 </svelte:head>
 
 <section class="">
-  <h1>Recent posts</h1>
+  <h1>Blog</h1>
 
   <ul>
-    {#each posts.items as post}
+    {#each posts.items as { id, title, date_published }}
       <!-- we're using the non-standard `rel=prefetch` attribute to
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-      <li>
-        <h2><a rel="prefetch" href={`/blog/${post.id}`}>{post.title}</a></h2>
-        <div><time>{post.date_published}</time></div>
+      <li class="mb-8">
+        <h2><a rel="prefetch" href={`/blog/${id}`}>{title}</a></h2>
+        <div><time>{new Date(date_published).toString()}</time></div>
       </li>
     {/each}
   </ul>
