a901c724816e318a440dd9cd8d8e1dff24c05789
Russell Jones <jonesrussell42@gmail.com>
2021-02-21 15:27:46 -0500

    WIP

--- src/routes/blog/[id].svelte
+++ src/routes/blog/[id].svelte
@@ -7,14 +7,15 @@
       .then((r: any) => r.json())
       .then((feed: any) => {
         const post = feed.items.filter((item: any) => item.id == id)[0];
-
+        console.log('post', post);
         // Convert images to full urls
-        const regex = /src=\\*"(.*)\\*"/gi;
+        /*
+        const regex = /src=\"(.*)\"/gi;
         post.content_html = post.content_html.replaceAll(
           regex,
-          'src="https://blog.jonesrussell42.xyz$1"'
+          'src="https://blog.jonesrussell42.xyz/$1"'
         );
-
+*/
         return { post };
       });
   }
