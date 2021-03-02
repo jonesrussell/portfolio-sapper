0d08564537e7ca7dbe0df576cf7f123e184a5f2a
Russell Jones <jonesrussell42@gmail.com>
2021-03-02 22:41:59 +0000

    Fixed route to contact process

--- src/routes/process/contact.js
+++ src/routes/process/contact.js
@@ -2,6 +2,7 @@ export async function post(req, res, next) {
   res.setHeader('Content-Type', 'application/json')
   const data = req.body
   // Do something with the data...
+  console.debug(data)
 
   return res.end(JSON.stringify({ success: true }))
 }
